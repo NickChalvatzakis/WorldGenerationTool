630c964c888a13166c06772d512efa7b9eac40d8
NickCh1996 <foouli@hotmail.com>
2025-12-25 19:48:52 +0200

    first commit

---
+++ Assets/Scripts/CozyWorldGeneration/Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CozyWorldGeneration
+{
+    public abstract class Grid<T> where T : class
+    {
+        protected Dictionary<Vector2Int, T> tiles;
+        
+        public int Width {get; protected set;}
+        public int Height {get; protected set;}
+
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            tiles = new Dictionary<Vector2Int, T>();
+        }
+
+        public T GetTile(Vector2Int position)
+        {
+            tiles.TryGetValue(position, out T tile);
+            return tile;
+        }
+
+        public virtual void SetTile(int x, int y, T tile)
+        {
+            var key = new Vector2Int(x, y);
+            if (tile == null)
+            {
+                tiles.Remove(key);
+            }
+            else
+            {
+                tiles[key] = tile;
+            }
+        }
+        
+        
+        
+        
+    }
+    
+}
